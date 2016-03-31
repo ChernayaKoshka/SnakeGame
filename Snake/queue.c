d038d6eaf8be6d205dd2f2aaaf3aacdab7600bbb
Christopher A <chris.altig@gmail.com>
2016-03-31 12:38:20 -0700

    Removed debug function

--- Snake/queue.c
+++ Snake/queue.c
@@ -25,58 +25,8 @@ POINT pop()
 
 int CalculateIndex(int count)
 {
-	#if DEBUG
-		LogCalculateIndex(count);
-	#endif
 	if (queue.front - count + 1 < 0)
 		return MaxQueueSize + queue.front - count + 1;
 	else
 		return queue.front - count + 1;
-}
-
-char* fmt0 = "v\r[0][1][2][3][4]\r\n";
-char* fmt1 = "    v\r[0][1][2][3][4]\r\n";
-char* fmt2 = "       v\r[0][1][2][3][4]\r\n";
-char* fmt3 = "          v\r[0][1][2][3][4]\r\n";
-char* fmt4 = "             v\r[0][1][2][3][4]\r\n";
-static FILE* fp;
-void OpenLog()
-{
-	fopen_s(&fp, "log.txt", "w");
-}
-
-void LogCalculateIndex(int count)
-{
-	char* fmt = "Front: %d\r\nCount: %d\r\nIndex: %d\r\n";
-
-	fprintf_s(fp, "0-----------------------0\r\n");
-		int num = 0;
-		if (queue.front - count +1 < 0)
-			num =  MaxQueueSize + queue.front - count + 1;
-		else
-			num =  queue.front - count + 1;
-
-		fprintf_s(fp, "1-----------------------1\r\n");
-		fprintf_s(fp, fmt, queue.front, count, num);
-		switch (num)
-		{
-		case 0:
-			fprintf_s(fp, fmt0);
-			break;
-		case 1:
-			fprintf_s(fp, fmt1);
-			break;
-		case 2:
-			fprintf_s(fp, fmt2);
-			break;
-		case 3:
-			fprintf_s(fp, fmt3);
-			break;
-		case 4:
-			fprintf_s(fp, fmt4);
-			break;
-		}
-		fprintf_s(fp, "1-----------------------1\r\n");
-	
-	fprintf_s(fp, "0-----------------------0\r\n");
 }
